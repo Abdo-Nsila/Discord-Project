28cbe1a46c19a2c798568ed42a59ee10f1f60822
abdo <abdonsila2004@gmail.com>
2023-09-03 22:17:21 +0100

    ADD scroll features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useRef } from "react";
 import Cards from "./Components/Cards";
 import { library } from "./data";
 
 export default function App() {
 
+    const topButton = useRef();
+    const bottomButton = useRef();
+
+    function moveDown(){
+        bottomButton.current.scrollIntoView({
+            behavior:"smooth",
+        })
+    };
+    function moveTop(){
+        topButton.current.scrollIntoView({
+            behavior:"smooth",
+        })
+    };
+
     const showData = library.map((item, index) => <Cards key={index} id={item.id} title={item.title} desc={item.desc} rating={item.rating} prix={item.prix} srcImg={item.srcImg} />);
 
-    return <div className="all-card">{showData}</div>
+    return (
+        <>
+            <button ref={topButton} onClick={moveDown}>Go Down</button>
+            <div className="all-card">{showData}</div>
+            <button ref={bottomButton} onClick={moveTop}>Go Top</button>
+        </>
+    )
 }
