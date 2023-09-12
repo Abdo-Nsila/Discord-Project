66a1a9b29dcae8555578e87fe882c9943db871e7
abdo <abdonsila2004@gmail.com>
2023-09-12 20:51:44 +0100

    UPDATE some Files

--- src/Components/MainServer/Categories/Categories.jsx
+++ src/Components/MainServer/Categories/Categories.jsx
@@ -2,15 +2,20 @@ import './Categories.css'
 import Chats from './Chats/Chats'
 
 export default function Categories({ name }){
-    function handleFocus(e,color){
-        e.target.style.backgroundColor = color;
+
+    function handleEvent(e,color){
+        const block = e.target;
+        block.addEventListener('focus',()=>{
+            block.target.style.backgroundColor = color;
+        })
     }
+
     return(
         <div className='categorie'>
             <span>{name}</span>
             <div className='chats'>
-                <Chats chatType={'chat'} name={'📋-chat'} handleFocus={handleFocus}/>
-                <Chats chatType={'voice'} name={'📚 Voice Chat'} handleFocus={handleFocus}/>
+                <Chats chatType={'chat'} name={'📋-chat'} handleEvent={handleEvent}/>
+                <Chats chatType={'voice'} name={'📚 Voice Chat'} handleEvent={handleEvent}/>
             </div>
         </div>
     )
